7a5eb1940e5a2963f7bd976043c20d933f131f33
Benjamin Jasper <jasper.benni@gmail.com>
2023-02-04 08:45:28 +0100

    chore: upgraded dependecies

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
-import solidSvg from 'vite-plugin-solid-svg';
+import { defineConfig } from 'vite'
+import solidPlugin from 'vite-plugin-solid'
+import solidSvg from 'vite-plugin-solid-svg'
 
 export default defineConfig({
-  plugins: [solidPlugin(), solidSvg()],
-  server: {
-    port: 3000,
-  },
-  build: {
-    target: 'esnext',
-  },
-});
+	plugins: [solidPlugin(), solidSvg()],
+	server: {
+		port: 3000,
+	},
+	build: {
+		target: 'esnext',
+	},
+})
